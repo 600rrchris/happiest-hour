bb94bc1b3171fe7ed8d84b557eba9bbd9e1d8c5f
MacBook Pro <600rrchris@gmail.com>
2019-09-12 12:38:16 -0500

    models needed edits

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=250)
+
+class Group(models.Model):
+    user = models.ManyToManyRel(User)
+    name = models.CharField(max_length=250)
+
+class Event(models.Model):
+    title : models.CharField(max_length=250)
+    group : models.ManyToOneRel(Group)
+    location : models.URLField(max_length=200)
+    time :  models.TimeField(auto_now=False, auto_now_add=False,)
+    date : models.DateField(auto_now=False, auto_now_add=False,)
+
+class Comments(models.Model):
+    user : models.OneToOneRel(User)
+    content: models.TextField(?)
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
-def home(request):
-    return HttpResponse('<h1>Happiest hour</h1>')
+
 def users(request):
     return render(request, 'users.html')
 def groups(request):
