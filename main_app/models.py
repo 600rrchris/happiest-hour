c330a3c98bbea842c1e0ca6672b9d3c7c03b7933
MacBook Pro <600rrchris@gmail.com>
2019-09-13 16:59:59 -0500

    fixed models

--- main_app/models.py
+++ main_app/models.py
@@ -9,12 +9,12 @@ class Group(models.Model):
 
 
 class Event(models.Model):
-    title : models.CharField(max_length=250)
-    group : models.ManyToManyField(Group)
-    location : models.URLField(max_length=200)
-    time :  models.TimeField(auto_now=False, auto_now_add=False,)
-    date : models.DateField(auto_now=False, auto_now_add=False,)
+    title = models.CharField(max_length=250)
+    group = models.ManyToManyField(Group)
+    location = models.URLField(max_length=200)
+    time = models.TimeField(auto_now=False, auto_now_add=False,)
+    date = models.DateField(auto_now=False, auto_now_add=False,)
 
 class Comment(models.Model):
-    user : models.ManyToManyField(User)
-    content: models.TextField(max_length=250)
+    user = models.ManyToManyField(User)
+    content = models.TextField(max_length=250)
