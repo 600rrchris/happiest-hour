50d60ccee8c5575ba71304f7da3d975202e93dc6
MacBook Pro <600rrchris@gmail.com>
2019-09-13 16:36:47 -0500

    events paths made

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('users/', views.users, name='users'),
     path('groups/', views.groups_index, name='groups'),
+    path('accounts/signup', views.signup, name='signup'),
+    path('events/index', views.events_index, name='events_index'),
+    path('events/new', views.events_new, name='events_new'),
+    path('events/details', views.events_details, name='events_details')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .models import User, Group, Event, Comment
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Happiest Hour</h1>')
+    return render(request, 'home.html')
+
+def events_index(request):
+    return render(request, 'events/index.html')
+
+def events_new(request):
+    return render(request, 'events/new.html')
+
+def events_details(request):
+    return render(request, 'events/details.html')    
+
 def users(request):
     return render(request, 'users.html')
 
@@ -10,6 +22,17 @@ def groups_index(request):
     groups = Group.objects.all()
     return render(request, 'groups.html', {'groups' : groups})
 
-def home(request):
-  return render(request, 'home.html')
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('groups')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
 
