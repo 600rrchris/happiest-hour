aa562dce8bb3d0c260d3269a2ec9c99074fb1f54
MacBook Pro <600rrchris@gmail.com>
2019-09-13 12:03:21 -0500

    test

--- main_app/views.py
+++ main_app/views.py
@@ -4,5 +4,11 @@ from .models import User, Group, Event, Comment
 
 def users(request):
     return render(request, 'users.html')
+
 def groups(request):
     return render(request, 'groups.html')
+
+def home(request):
+  return render(request, 'home.html')
+
+hello
