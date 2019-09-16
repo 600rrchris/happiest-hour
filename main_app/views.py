df8f9a8dddfe058cf963bcf59ce958bf863c5a21
beverlyosoria <bevosoria@gmail.com>
2019-09-16 15:43:37 -0500

    updates group_form and views

--- main_app/views.py
+++ main_app/views.py
@@ -68,7 +68,7 @@ class GroupUpdate(LoginRequiredMixin, UpdateView):
 
 class EventCreate(LoginRequiredMixin, CreateView):
     model = Event
-    fields = ['title', 'location', 'time', 'date']
+    fields = ['title',[Group], 'time', 'date', 'location', 'Comment']
 
 
 class EventDelete(LoginRequiredMixin, DeleteView):
