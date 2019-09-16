674c121609ba4411e1ddf1a72f68ca55a26b02e3
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-09-16 12:21:16 -0500

    update admin with Poll model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Group, User, Comment, Event
+from .models import Group, User, Comment, Event, Poll
 # Register your models here.
 admin.site.register(Group)
 admin.site.register(Comment)
 admin.site.register(Event)
+admin.site.register(Poll)
+
