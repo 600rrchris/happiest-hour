ab230d88c5bf289107accf5e87a9f34ce1fff3d2
MacBook Pro <600rrchris@gmail.com>
2019-09-18 12:13:36 -0500

    custom user and custom signup page dont act like youre not impressed

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,1 +1,10 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import Account, MyAccountManager
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = Account
+        fields = ('username', 'email')
