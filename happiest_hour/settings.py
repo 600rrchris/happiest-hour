1c352254e9f1bc592b37d5a384fcd2c05510ce3a
MacBook Pro <600rrchris@gmail.com>
2019-09-18 14:06:37 -0500

    Config deployment

--- happiest_hour/settings.py
+++ happiest_hour/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +119,5 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+import django_heroku
+django_heroku.settings(locals())
